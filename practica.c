abd4a270295bb7ee731b93949cb87d56a71698fa
Marcos Arnez <marcosarnezpocomani@gmail.com>
2026-08-25 10:24:31 -0400

    feat: practica union bit-fields y control de desborde

---
+++ practica.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    int8_t a = 120, b = 20;
+    int8_t resultado = a + b;
+
+    printf("%d + %d = %d\n", a, b, resultado);
+    return 0;
+}
